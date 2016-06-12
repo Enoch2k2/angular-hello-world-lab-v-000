93702b125aa5d5dedd473a0811b7bb7be1974c0b
Enoch Griffith <enoch2k2@hotmail.com>
2016-06-11 22:28:45 -0500

    Redone

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -1,6 +1,6 @@
 function MainController($scope) {
-  $scope.name = "Casey"
-  $scope.email = "Casey@gooberbutt.com"
+  $scope.name = "Enoch";
+  $scope.email = "Enoch2k2@email.com";
   $scope.phone = "555-555-5555"
 }
 
